ef05f62514ad8b59fc5b2f5d3b00969ebf39f71c
Siv-tspab <sbapst.j@gmail.com>
2022-01-28 19:08:24 +0100

    ADD: list & navbar to main page

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,7 @@
 import { useEffect, useState } from "react";
 import Config from "./api/api.config.json"
+import List from "./components/List/List";
+import NavBar from "./components/NavBar/NavBar";
 import { User } from "./models/User";
 
 function App() {
@@ -14,7 +16,11 @@ function App() {
 
 
   return (
-    <div className="App">
+    <div className="App min-h-screen bg-slate-900 text-white">
+      <NavBar/>
+      <div className="flex flex-wrap justify-center w-10/12 m-auto">
+        <List users={users} />
+      </div>
     </div>
   );
 }
