f4ecbd18fa4bd023e69d1e63a66db9e44e31f5a4
Siv-tspab <sbapst.j@gmail.com>
2022-01-28 19:07:12 +0100

    ADD: fetch API + create User's model

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,18 @@
+import { useEffect, useState } from "react";
+import Config from "./api/api.config.json"
+import { User } from "./models/User";
 
 function App() {
+
+  const [users, setUsers] = useState<User[]>([]);
+
+  useEffect(() => {
+    fetch(Config.url + "?results=" + Config.userNumber)
+      .then(response => response.json())
+      .then(res => setUsers(res.results))
+  }, []);
+
+
   return (
     <div className="App">
     </div>
---
+++ src/models/User.ts
@@ -0,0 +1,33 @@
+export interface User {
+    cell: "414-347-6911";
+    dob: { age: number; date: Date };
+    email: string;
+    gender: string;
+    id: { name: string; value: any };
+    location: {
+        city: string;
+        coordinates: { latitude: string; longitude: string };
+        country: string;
+        postcode: string;
+        state: string;
+        street: { number: number; name: string };
+        timezone: { offset: string; description: string };
+    };
+    name: {
+        first: string;
+        last: string;
+        title: string;
+    };
+    nat: string;
+    phone: string;
+    age: number;
+    picture: {
+        large: string;
+        medium: string;
+        thumbnail: string;
+    };
+    registered: {
+        age: number;
+        date: Date;
+    };
+}
